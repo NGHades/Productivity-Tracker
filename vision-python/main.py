7bd42901a5c6a7544fedb7aa189836d63c5346e1
Richie Nguyen <146700140+NGHades@users.noreply.github.com>
2025-12-02 21:58:15 -0800

    Webcam Connection - webcam frame is captured in main.py execution

--- vision-python/main.py
+++ vision-python/main.py
@@ -1,1 +1,19 @@
 import cv2
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error: could not access webcam")
+else:
+    print("Webcam accessed successfully")
+
+ret, frame = cap.read()
+
+if ret:
+    cv2.imshow("Captured Frame", frame)
+    cv2.waitKey(0) # Wait for a key press to close the window
+    cv2.destroyAllWindows()
+else:
+    print("Could not capture any frames")
+
+cap.release()
