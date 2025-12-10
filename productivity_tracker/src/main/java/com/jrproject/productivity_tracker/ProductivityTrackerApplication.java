acedf539e8732813751ac810fa3670bd80957432
JohnHPham <phamjohn752@gmail.com>
2025-12-09 21:57:24 -0800

    FIX: replaced backend with new SpringBoot set up

---
+++ productivity_tracker/src/main/java/com/jrproject/productivity_tracker/ProductivityTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.jrproject.productivity_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductivityTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductivityTrackerApplication.class, args);
+	}
+
+}
